eb0d9e4cd405aea5c80d3cfffc26b5d84e182878
Andrex2605 <andresarispe2605@gmail.com>
2024-05-30 09:54:44 -0400

    projecto

---
+++ lib/home_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () async {
+                  final video = File(await pickVideo(context));
+                    if (await video.exists()) {
+                      uploadVideo(video);
+                    } else {
+                      print('Video not found');
+                    }
+                }, 
+                child: const Text('Subir archivo', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)
+              ),
+              const SizedBox(height: 20),
+              FilledButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/video');
+                  },
+                  child: const Text('Enviar',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18))),
+            ],
+          ),
+        ),
+      );
+  }
+}
+
+
+ Future<void> uploadVideo(File videoFile) async {
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://10.12.25.73:3000/upload'),
+    );
+    request.files
+        .add(await http.MultipartFile.fromPath('video', videoFile.path));
+    var response = await request.send();
+    if (response.statusCode == 201) {
+      print('Video uploaded successfully');
+    } else {
+      //TODO: manage error. At this point, it would be probably a communication error
+      print('Failed to upload video');
+    }
+  }
+
+  Future<String> pickVideo(BuildContext context) async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.video,
+        allowMultiple: false,
+      );
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+        String? filePath = file.path;
+
+        if (filePath != null) {
+          return filePath;
+        } else {
+          //TODO: manage error in all these else's
+          return '';
+        }
+      } else {
+        return '';
+      }
+    } catch (e) {
+      print('Error picking video: $e');
+      return '';
+    }
+  }
+
---
+++ lib/video_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key});
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late final FlickManager flickManager;
+
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(videoPlayerController: VideoPlayerController.networkUrl(Uri.parse('http://10.12.25.73:3000/download')));
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text('Video Sin fondo'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: SizedBox(
+                      height: 350,
+                      width: 380,
+                      child: FlickVideoPlayer(flickManager: flickManager))),
+              const SizedBox(height: 30),
+              FilledButton(
+                  onPressed: () {
+                    FileDownloader.downloadFile(
+                        url: 'http://10.12.25.73:3000/download',
+                        name: 'Prueba1', //(optional)
+                        onProgress: (String? fileName, double progress) {
+                          print('FILE fileName HAS PROGRESS $progress');
+                        },
+                        onDownloadCompleted: (String path) {
+                          print('FILE DOWNLOADED TO PATH: $path');
+                        },
+                        onDownloadError: (String error) {
+                          print('DOWNLOAD ERROR: $error');
+                        });
+                  },
+                  child: const Text('Descargar'))
+            ]),
+          ),
+        ));
+  }
+}
